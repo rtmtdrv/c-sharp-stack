4ef3beeac8b2084ec3afedd6719ceda231c19b65
rtmtdrv <art.todorov@me.com>
2019-08-15 14:37:29 -0700

    adding dojosurveymodels

---
+++ DojoSurveyModel/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyModel.Models;
+
+namespace DojoSurveyModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost("survey")]
+        public IActionResult Survey(Survey survey)
+        {
+            System.Console.WriteLine(survey);
+            return RedirectToAction("Result", survey);
+        }
+
+        public IActionResult Result(Survey survey)
+        {
+            System.Console.WriteLine(survey);
+            return View("Result", survey);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
