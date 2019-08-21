4b0c7f0a176ae25de5e76f5fe94555e0d5e30db5
rtmtdrv <art.todorov@me.com>
2019-08-20 19:28:13 -0700

    adding CRUDelicious

---
+++ CRUDelicious/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CRUDelicious.Models;
+
+namespace CRUDelicious.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext dbContext;
+        public HomeController(MyContext context)
+        {
+            dbContext = context;
+        }
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.AllDishes = dbContext.Dishes;
+            return View();
+        }
+
+        [HttpGet("new")]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(Dish newDish)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Dishes.Add(newDish);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("new");
+        }
+
+        [HttpGet("{dishId}")]
+        public IActionResult Show(int dishId)
+        {   
+            Dish thisDish = dbContext.Dishes.FirstOrDefault(d => d.DishId == dishId);
+            return View(thisDish);
+        }
+
+        [HttpGet("edit/{dishId}")]
+        public IActionResult Edit(int dishId)
+        {
+            Dish thisDish = dbContext.Dishes.FirstOrDefault(d => d.DishId == dishId);
+            return View(thisDish);
+        }
+
+        [HttpPost("update/{dishId}")]
+        public IActionResult Update(Dish thisDish, int dishId)
+        {
+            Dish toUpdate = dbContext.Dishes.FirstOrDefault(d => d.DishId == dishId);
+            if(ModelState.IsValid)
+            {
+                toUpdate.Chef = thisDish.Chef;
+                toUpdate.Name = thisDish.Name;
+                toUpdate.Calories = thisDish.Calories;
+                toUpdate.Tastiness = thisDish.Tastiness;
+                toUpdate.Description = thisDish.Description;
+                toUpdate.UpdatedAt = DateTime.Now;
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Edit");
+        }
+
+        [HttpPost("delete/{dishId}")]
+        public IActionResult Delete(int dishId)
+        {
+            Dish toDelete = dbContext.Dishes.FirstOrDefault(d => d.DishId == dishId);
+            dbContext.Dishes.Remove(toDelete);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUDelicious/Models/Dish.cs
@@ -0,0 +1,32 @@
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDelicious.Models
+{
+    public class Dish
+    {
+        [Key]
+        public int DishId {get; set;}
+        [Required]
+
+        [Display(Name="Chef's Name")]
+        public string Chef {get; set;}
+        [Required]
+
+        [Display(Name="Name of Dish")]
+        public string Name {get; set;}
+        [Required]
+
+        [Display(Name="# of Calories")]
+        public int Calories {get; set;}
+        [Required]
+
+        public int Tastiness {get; set;}
+        [Required]
+
+        public string Description {get; set;}
+        public DateTime CreatedAt {get; set;} = DateTime.Now;
+        public DateTime UpdatedAt {get; set;} = DateTime.Now;
+    }
+}
